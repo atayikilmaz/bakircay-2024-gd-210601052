7c402de2e36cc1a00de39d814e786e5ab2afbc67
atayikilmaz <ata.yikilmaz@gmail.com>
2024-11-22 13:49:25 +0300

    implemente matching logic

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -2,43 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Items : MonoBehaviour
+
+public class CubeDragHandler : MonoBehaviour
 {
     Rigidbody rb;
+    Vector3 ScreenPoint;
+    Vector3 Offset;
 
-    Vector3 screenPoint;
-    Vector3 offset;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
-    private void OnMouseDown()
+    void OnMouseDown()
     {
         rb.useGravity = false;
-        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-
+        ScreenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        Offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, ScreenPoint.z));
     }
 
-
-    private void OnMouseDrag()
-    {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-
-        rb.position = curPosition;
-        rb.MovePosition(new Vector3(rb.position.x, 0.5f, rb.position.z));
+    private void OnMouseDrag() {
+        Vector3 cursorPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, ScreenPoint.z);
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + Offset;
+        rb.position = cursorPosition;
+        rb. MovePosition(new Vector3(rb.position.x, 0.5f, rb.position.z));
     }
 
-    private void OnMouseUp()
-    {
+    private void OnMouseUp() {
         rb.useGravity = true;
     }
 }
